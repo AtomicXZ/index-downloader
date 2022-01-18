580767f269cf077cea60c7ff9ef7ef64244cc32a
AtomicXZ <achubadyal4@gmail.com>
2022-01-18 11:27:27 +0530

    auto format with PEP-8
    Signed-off-by: AtomicXZ <achubadyal4@gmail.com>

--- main.py
+++ main.py
@@ -26,7 +26,8 @@ indexLink = link[:link.replace("https://", "").index("/")+8]
 options = Options()
 options.add_argument('--headless')
 options.add_argument('--disable-gpu')
-driver = Chrome(options = options)
+driver = Chrome(options=options)
+
 
 def getSoup(link):
     global soup
@@ -34,7 +35,8 @@ def getSoup(link):
     while True:
         driver.get(link)
         try:
-            myElem = WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".list-group-item.list-group-item-action")))
+            myElem = WebDriverWait(driver, 3).until(EC.presence_of_element_located(
+                (By.CSS_SELECTOR, ".list-group-item.list-group-item-action")))
             break
         except TimeoutException:
             print("Connection Failed, retrying. Please Wait!")
@@ -54,41 +56,46 @@ def download(Sno, Dlist):
         except ValueError:
             pass
         while True:
-            os.system(f"aria2c \"{i}\" -d\"{dir}\" --auto-file-renaming=false --save-session log-{Sno}.txt")    
+            os.system(
+                f"aria2c \"{i}\" -d\"{dir}\" --auto-file-renaming=false --save-session log-{Sno}.txt")
             if os.path.isfile(file):
                 break
             elif os.stat(f"log-{Sno}.txt").st_size == 0:
                 break
-    
+
     # delete the generated log file
     try:
         os.remove(f"log-{Sno}.txt")
     except FileNotFoundError:
         pass
 
+
 # fetch all links from the base url
 getSoup(link)
-allFiles = soup.find_all("a", {"class" : "list-group-item-action"}, href = True)
+allFiles = soup.find_all("a", {"class": "list-group-item-action"}, href=True)
 
 ddlLink = []
 for i in allFiles:
     if not i["href"].replace('?a=view', '')[-1] == "/":
         # add link to list after formatting if it's a file
-        ddlLink.append(f"{indexLink}{i['href'].replace('?a=view', '').replace(' ', '%20')}")
+        ddlLink.append(
+            f"{indexLink}{i['href'].replace('?a=view', '').replace(' ', '%20')}")
     else:
         # open the link if it's not a file and add it to allFiles var
-        getSoup(f"{indexLink}{i['href'].replace('?a=view', '').replace(' ', '%20')}")
-        allFiles.extend(soup.find_all("a", {"class" : "list-group-item-action"}, href = True))
+        getSoup(
+            f"{indexLink}{i['href'].replace('?a=view', '').replace(' ', '%20')}")
+        allFiles.extend(soup.find_all(
+            "a", {"class": "list-group-item-action"}, href=True))
 
 driver.close()
 
-# separate the links list 
+# separate the links list
 k, m = divmod(len(ddlLink), 4)
 ddlList = [ddlLink[i*k+min(i, m):(i+1)*k+min(i+1, m)] for i in range(4)]
 
 # downloading starts here
 if __name__ == '__main__':
-    Process(target = download, args = (1, ddlList[0],)).start()
-    Process(target = download, args = (2, ddlList[1],)).start()
-    Process(target = download, args = (3, ddlList[2],)).start()
-    Process(target = download, args = (4, ddlList[3],)).start()
+    Process(target=download, args=(1, ddlList[0],)).start()
+    Process(target=download, args=(2, ddlList[1],)).start()
+    Process(target=download, args=(3, ddlList[2],)).start()
+    Process(target=download, args=(4, ddlList[3],)).start()
