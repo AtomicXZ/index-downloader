fe041305b83342e6e155004a3ece154ce7f55cb6
AtomicXZ <achubadyal4@gmail.com>
2022-01-30 00:37:22 +0530

    remove ddos field from json file and newCred script - there's no support for ddos index links for now anyways, so there's no point in keeping the ddos field - might add support in the future, if i do then this commit will be reverted then, but let's just remove the stuff for now
    Signed-off-by: AtomicXZ <achubadyal4@gmail.com>

--- newCred.py
+++ newCred.py
@@ -7,14 +7,9 @@ name = input("Enter keyword from index link:  ")
 user = input("Enter username for the index:  ")
 password = input("Enter password for the index:  ")
 
-ddos = input("Is Ddos enabled for the index (T/F):  ")
-if ddos.lower() == "t":
-    ddos = True
-else:
-    ddos = False
 
 data = json.load(open(path))
-data[name.lower()] = {"user": user, "password": password, "ddos": ddos}
+data[name.lower()] = {"user": user, "password": password}
 
 with open(path, "w", encoding="utf-8") as f:
     json.dump(data, f, ensure_ascii=False, indent=4)
